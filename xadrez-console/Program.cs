9fa8cfed53be777fa9680f03bf588ab2b1e65aa8
Juan <juanmendespeixoto@gmail.com>
2023-11-06 09:54:27 -0300

    adicionando cor as pecas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -5,11 +5,17 @@ using xadrez_console.xadrez;
 namespace xadrez_console {
     internal class Program {
         static void Main(string[] args) {
+            try {
+                Tabuleiro tab = new Tabuleiro(8,8);
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 5));
+                Tela.ImprimirTabuleiro(tab);    
+            } catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
         }
